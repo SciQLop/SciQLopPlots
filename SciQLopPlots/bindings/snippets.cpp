688fa9d06618cbf9ea172b3bec3ce66b189f702c
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-03-23 16:24:32 +0100

    Releases GIL during graph data update
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLopPlots/bindings/snippets.cpp
+++ SciQLopPlots/bindings/snippets.cpp
@@ -1,6 +1,7 @@
 // @snippet QCPGraph-setData
 NpArray_view x{pyArgs[0]};
 NpArray_view y{pyArgs[1]};
+Py_BEGIN_ALLOW_THREADS
 QVector<QCPGraphData> data(x.flat_size());
 const auto x_data = x.data();
 const auto y_data = y.data();
@@ -9,6 +10,7 @@ for(auto i=0UL;i<x.flat_size();i++)
     data[i]={x_data[i],y_data[i]};
 }
 %CPPSELF.data()->set(std::move(data),true);
+Py_END_ALLOW_THREADS
 // @snippet QCPGraph-setData
 
 // @snippet QCPGraph-setSelected
