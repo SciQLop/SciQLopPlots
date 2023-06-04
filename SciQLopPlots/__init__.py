ffce4a94787c61b4f31fbaa7124eb569ac44a085
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2023-06-04 19:17:54 +0200

    Bump version: 0.3.4 → 0.4.0

--- SciQLopPlots/__init__.py
+++ SciQLopPlots/__init__.py
@@ -2,4 +2,4 @@ from PySide6 import QtCore, QtGui, QtWidgets
 from .SciQLopPlotsBindings import *
 from .SciQLopPlotsBindings import _QCustomPlot as QCustomPlot
 
-__version__ = '0.3.4'
+__version__ = '0.4.0'
