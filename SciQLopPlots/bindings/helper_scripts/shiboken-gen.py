4fadffd58362e02dae54d1017519da4754538f3d
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2024-06-02 14:09:48 +0200

    Sets automatically LLVM_INSTALL_DIR
    Signed-off-by: Alexis Jeandet <alexis.jeandet@member.fsf.org>

--- SciQLopPlots/bindings/helper_scripts/shiboken-gen.py
+++ SciQLopPlots/bindings/helper_scripts/shiboken-gen.py
@@ -19,6 +19,7 @@ parser.add_argument('--build-directory')
 parser.add_argument('--ref-build-target')
 args = parser.parse_args()
 
+env = os.environ.copy()
 
 def cpp_flags(build_dir, ref_build_target):
     with open(f"{build_dir}/meson-info/intro-targets.json", 'r') as targets_f:
@@ -35,33 +36,38 @@ shiboken_constant_args=['--generator-set=shiboken',
     '--use-isnull-as-nb_nonzero',
     '--avoid-protected-hack',
     '--enable-pyside-extensions',
-    '--debug-level=full',
+    #'--debug-level=full',
     '-std=c++17',
     '--generator-set=shiboken']
 
-if 'linux' in platform.system().lower() and os.environ.get('AUDITWHEEL_PLAT', '') == 'manylinux_2_28_x86_64':
-    gcc_found = False
-    for v in reversed(range(11,14)):
-        prefix = f'/opt/rh/gcc-toolset-{v}/root/usr'
-        if os.path.exists(prefix):
-            shiboken_constant_args += [
-                f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}",
-                f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}/x86_64-redhat-linux",
-                f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}/backward",
-                f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/include",
-                f"-I/opt/rh/gcc-toolset-13/root/usr/include",
-                f"-I/usr/local/include",
-                f"-I/usr/include",
-            ]
-            gcc_found = True
-            shiboken_constant_args += ['--compiler=g++']
-            break
-
+if 'linux' in platform.system().lower():
+    if os.environ.get('AUDITWHEEL_PLAT', '') == 'manylinux_2_28_x86_64':
+        gcc_found = False
+        for v in reversed(range(11,14)):
+            prefix = f'/opt/rh/gcc-toolset-{v}/root/usr'
+            if os.path.exists(prefix):
+                shiboken_constant_args += [
+                    f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}",
+                    f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}/x86_64-redhat-linux",
+                    f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/../../../../include/c++/{v}/backward",
+                    f"-I/opt/rh/gcc-toolset-{v}/root/usr/lib/gcc/x86_64-redhat-linux/{v}/include",
+                    f"-I/opt/rh/gcc-toolset-13/root/usr/include",
+                    f"-I/usr/local/include",
+                    f"-I/usr/include",
+                ]
+                gcc_found = True
+                shiboken_constant_args += ['--compiler=g++']
+                break
+    llvm_dir = subprocess.run(['llvm-config', '--prefix'], check=True, stdout=subprocess.PIPE)
+    if llvm_dir.returncode == 0:
+        llvm_dir = llvm_dir.stdout.decode().strip()
+        env['LLVM_INSTALL_DIR'] = llvm_dir
 
 cmd = [args.shiboken, args.input_header, args.input_xml ] + shiboken_constant_args + cpp_flags(args.build_directory, args.ref_build_target) + [ f'--typesystem-paths={args.typesystem_paths}', f'--output-directory={args.output_directory}']
 
 subprocess.run(
     cmd,
-    check=True
+    check=True,
+    env=env
 )
 
