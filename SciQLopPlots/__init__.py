a0775b97b0af3841066074a8fcce3be71b6a3cc5
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2025-06-25 10:29:18 +0200

    Bump version: 0.16.0 → 0.16.1

--- SciQLopPlots/__init__.py
+++ SciQLopPlots/__init__.py
@@ -11,7 +11,7 @@ import sys
 
 sys.modules["SciQLopPlotsBindings"] = SciQLopPlotsBindings
 
-__version__ = '0.16.0'
+__version__ = '0.16.1'
 
 def _merge_kwargs(kwargs, **kwargs2):
     for k, v in kwargs2.items():
