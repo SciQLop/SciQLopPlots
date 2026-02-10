9235893aa19dfee0deb4771e4ed77c3d042f0f66
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2026-02-10 18:53:40 +0100

    Bump Qt and PySide to 6.10.2

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-podman build -t  sciqlopplots_build --build-arg QT_VERSION=6.10.0  --build-arg PYTHON_VERSION=cp313-cp313 .
+podman build -t  sciqlopplots_build --build-arg QT_VERSION=6.10.2  --build-arg PYTHON_VERSION=cp313-cp313 .
--- Docker/make_wheel.sh
+++ Docker/make_wheel.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-podman run --rm -it -v $(pwd):/SciQLopPlots:z -e'PATH=/opt/Qt/6.10.0/gcc_64/bin/:$PATH' localhost/sciqlopplots_build:latest /opt/python/cp313-cp313/bin/python -m build -n /SciQLopPlots -Cdist-args=--allow-dirty -w
+podman run --rm -it -v $(pwd):/SciQLopPlots:z -e'PATH=/opt/Qt/6.10.2/gcc_64/bin/:$PATH' localhost/sciqlopplots_build:latest /opt/python/cp313-cp313/bin/python -m build -n /SciQLopPlots -Cdist-args=--allow-dirty -w
